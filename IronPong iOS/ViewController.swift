1a668e3fd234f761863ff52663c1ddd198e6743b
hjgoldman <haydengoldman@gmail.com>
2017-03-31 11:09:03 -0500

    Initial commit

---
+++ IronPong iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IronPong iOS
+//
+//  Created by Hayden Goldman on 3/31/17.
+//  Copyright © 2017 Hayden Goldman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
